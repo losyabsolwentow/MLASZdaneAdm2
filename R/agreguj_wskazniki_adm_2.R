8e3545b6b3c64faa3bc392cdb8e3f4e6bd957b75
losyabsolwentow <losyabsolwentow@ibe.edu.pl>
2022-11-03 10:42:30 +0100

    Ciąg dalszy poprawiania kompatybilności nazw argumentów i przekazywanych im obiektów

--- R/agreguj_wskazniki_adm_2.R
+++ R/agreguj_wskazniki_adm_2.R
@@ -10,8 +10,8 @@
 #' rundy monitoringu na danych administracyjnych
 #' @param grupy ramka danych zawierająca definicje podziałów na grupy -
 #' np. zwrócona przez funkcję \code{\link{utworz_grupowanie_ze_zmiennej}}
-#' @param rok rok, w którym grupa absolwentów uzyskała status absolwenta. Może
-#' to być więcej niż 1 wartość.
+#' @param rok_abso rok, w którym grupa absolwentów uzyskała status absolwenta.
+#' Może to być więcej niż 1 wartość.
 #' @param duplikaty wartość logiczna określająca czy należy odfiltrować ze
 #' zbioru duplikaty przy liczeniu niektórych wskaźników. Przyjmuje wartości:
 #' \itemize{
@@ -44,23 +44,23 @@
 #' }
 #' @export
 #' @importFrom dplyr %>% filter .data left_join
-agreguj_1rokpo_adm_2 = function(wsk2, wsk3, wsk4, grupy, rok, duplikaty = TRUE) {
+agreguj_1rokpo_adm_2 = function(wsk2, wsk3, wsk4, grupy, rok_abso, duplikaty = TRUE) {
   stopifnot(is.data.frame(wsk2),
             is.data.frame(wsk3),
             is.data.frame(wsk4),
             is.data.frame(grupy),
-            rok %in% c(2020, 2021) & length(rok) %in% 1,
+            rok_abso %in% c(2020, 2021) & length(rok_abso) %in% 1,
             c("id_szk", "id_abs", "rok_abs", "typ_szk", "teryt_woj", "branza") %in% names(wsk2),
             c("id_szk", "id_abs", "rok_abs", "typ_szk", "teryt_woj", "branza") %in% names(wsk3),
             c("id_szk", "id_abs", "rok_abs", "typ_szk", "teryt_woj", "branza") %in% names(wsk4),
             is.logical(duplikaty))
   
   wsk4 = wsk4 %>%
-    filter(.data$rok_abs %in% (rok))
+    filter(.data$rok_abs %in% (rok_abso))
   wsk3 = wsk3 %>%
-    filter(.data$rok_abs %in% (rok))
+    filter(.data$rok_abs %in% (rok_abso))
   wsk2 = wsk2 %>%
-    filter(.data$rok_abs %in% (rok))
+    filter(.data$rok_abs %in% (rok_abso))
   
   if (duplikaty) {
     dups = NULL
@@ -80,34 +80,34 @@ agreguj_1rokpo_adm_2 = function(wsk2, wsk3, wsk4, grupy, rok, duplikaty = TRUE) 
     liczebnosc_branze_ucz = liczebnosc_branze_ucz(.data))
   
   wskazniki_3 = agreguj_wskazniki(
-    wsk3, grupy, list("rok" = rok, "wsk2" = wsk2, "dups" = dups),
-    S3_01 = S3_mies(.data, min(rok), 1, max(rok), 1, dups),
-    S3_02 = S3_mies(.data, min(rok), 2, max(rok), 2, dups),
-    S3_03 = S3_mies(.data, min(rok), 3, max(rok), 3, dups),
-    S3_04 = S3_mies(.data, min(rok), 4, max(rok), 4, dups),
-    S3_05 = S3_mies(.data, min(rok), 5, max(rok), 5, dups),
-    S3_06 = S3_mies(.data, min(rok), 6, max(rok), 6, dups),
-    S3_07 = S3_mies(.data, min(rok), 7, max(rok), 7, dups),
-    S3_08 = S3_mies(.data, min(rok), 8, max(rok), 8, dups),
-    S3_09 = S3_mies(.data, min(rok), 9, max(rok), 9, dups),
-    S3_10 = S3_mies(.data, min(rok), 10, max(rok), 10, dups),
-    S3_11 = S3_mies(.data, min(rok), 11, max(rok), 11, dups),
-    S3_12 = S3_mies(.data, min(rok), 12, max(rok), 12, dups),
-    tab_s3_zaw = zawody_S3(.data, min(rok), 12, max(rok), 12, dups),
-    E2_nauka_kontyn = E2_nauka_kontyn(.data, rok, 12),
-    Z4_ucz = Z4_ods_prac_mies(.data, min(rok), 9, max(rok), 12, TRUE),
-    Z4_nie_ucz = Z4_ods_prac_mies(.data, min(rok), 9, max(rok), 12, FALSE),
-    Z8_formy_ucz = Z8_formy_prac_mies(.data, rok, 12, TRUE),
-    Z8_formy_nie_ucz = Z8_formy_prac_mies(.data, rok, 12, FALSE),
-    Z9_mlod_wrz = Z9_kont_mlod(.data, rok, 9),
-    W3_ucz = W3_sr_doch_uop(.data, rok, 9, 12, TRUE),
-    W3_nie_ucz = W3_sr_doch_uop(.data, rok, 9, 12, FALSE),
-    B2_bezrob = B2_ods_bezrob(.data, rok, 9, 12),
-    N2_biernosc = N2_ods_biernosc(.data, rok, 9, 12),
-    liczebnosc_branze_kont = liczebnosc_branze_kont(.data, wsk2, rok, 12),
-    liczebnosc_dziedziny = liczebnosc_dziedziny(.data, wsk2, rok, 12),
-    liczebnosc_dyscypliny = liczebnosc_dyscypliny(.data, wsk2, rok, 12),
-    dyscypliny_zawody = dyscypliny_zawody(.data, wsk2, rok, 12)
+    wsk3, grupy, list("rok_abso" = rok_abso, "wsk2" = wsk2, "dups" = dups),
+    S3_01 = S3_mies(.data, min(rok_abso), 1, max(rok_abso), 1, dups),
+    S3_02 = S3_mies(.data, min(rok_abso), 2, max(rok_abso), 2, dups),
+    S3_03 = S3_mies(.data, min(rok_abso), 3, max(rok_abso), 3, dups),
+    S3_04 = S3_mies(.data, min(rok_abso), 4, max(rok_abso), 4, dups),
+    S3_05 = S3_mies(.data, min(rok_abso), 5, max(rok_abso), 5, dups),
+    S3_06 = S3_mies(.data, min(rok_abso), 6, max(rok_abso), 6, dups),
+    S3_07 = S3_mies(.data, min(rok_abso), 7, max(rok_abso), 7, dups),
+    S3_08 = S3_mies(.data, min(rok_abso), 8, max(rok_abso), 8, dups),
+    S3_09 = S3_mies(.data, min(rok_abso), 9, max(rok_abso), 9, dups),
+    S3_10 = S3_mies(.data, min(rok_abso), 10, max(rok_abso), 10, dups),
+    S3_11 = S3_mies(.data, min(rok_abso), 11, max(rok_abso), 11, dups),
+    S3_12 = S3_mies(.data, min(rok_abso), 12, max(rok_abso), 12, dups),
+    tab_s3_zaw = zawody_S3(.data, min(rok_abso), 12, max(rok_abso), 12, dups),
+    E2_nauka_kontyn = E2_nauka_kontyn(.data, rok_abso, 12),
+    Z4_ucz = Z4_ods_prac_mies(.data, min(rok_abso), 9, max(rok_abso), 12, TRUE),
+    Z4_nie_ucz = Z4_ods_prac_mies(.data, min(rok_abso), 9, max(rok_abso), 12, FALSE),
+    Z8_formy_ucz = Z8_formy_prac_mies(.data, rok_abso, 12, TRUE),
+    Z8_formy_nie_ucz = Z8_formy_prac_mies(.data, rok_abso, 12, FALSE),
+    Z9_mlod_wrz = Z9_kont_mlod(.data, rok_abso, 9),
+    W3_ucz = W3_sr_doch_uop(.data, rok_abso, 9, 12, TRUE),
+    W3_nie_ucz = W3_sr_doch_uop(.data, rok_abso, 9, 12, FALSE),
+    B2_bezrob = B2_ods_bezrob(.data, rok_abso, 9, 12),
+    N2_biernosc = N2_ods_biernosc(.data, rok_abso, 9, 12),
+    liczebnosc_branze_kont = liczebnosc_branze_kont(.data, wsk2, rok_abso, 12),
+    liczebnosc_dziedziny = liczebnosc_dziedziny(.data, wsk2, rok_abso, 12),
+    liczebnosc_dyscypliny = liczebnosc_dyscypliny(.data, wsk2, rok_abso, 12),
+    dyscypliny_zawody = dyscypliny_zawody(.data, wsk2, rok_abso, 12)
   )
   
   wskazniki_4$grupy = wskazniki_4$grupy %>%
@@ -129,8 +129,8 @@ agreguj_1rokpo_adm_2 = function(wsk2, wsk3, wsk4, grupy, rok, duplikaty = TRUE) 
 #' rundy monitoringu na danych administracyjnych
 #' @param grupy ramka danych zawierająca definicje podziałów na grupy -
 #' np. zwrócona przez funkcję \code{\link{utworz_grupowanie_ze_zmiennej}}
-#' @param rok rok, w którym grupa absolwentów uzyskała status absolwenta. Może
-#' to być więcej niż 1 wartość.
+#' @param rok_abso rok, w którym grupa absolwentów uzyskała status absolwenta.
+#' Może to być więcej niż 1 wartość.
 #' @param duplikaty wartość logiczna określająca czy należy odfiltrować ze
 #' zbioru duplikaty przy liczeniu niektórych wskaźników. Przyjmuje wartości:
 #' \itemize{
@@ -156,19 +156,19 @@ agreguj_1rokpo_adm_2 = function(wsk2, wsk3, wsk4, grupy, rok, duplikaty = TRUE) 
 #' }
 #' @export
 #' @importFrom dplyr %>% filter .data left_join
-agreguj_aneks_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok, duplikaty = TRUE) {
+agreguj_aneks_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok_abso, duplikaty = TRUE) {
   stopifnot(is.data.frame(wsk3),
             is.data.frame(wsk4),
             is.data.frame(grupy),
-            rok %in% c(2020, 2021) & length(rok) %in% 1,
+            rok_abso %in% c(2020, 2021) & length(rok_abso) %in% 1,
             c("id_szk", "id_abs", "rok_abs", "typ_szk", "teryt_woj") %in% names(wsk3),
             c("id_szk", "id_abs", "rok_abs", "typ_szk", "teryt_woj") %in% names(wsk4),
             is.logical(duplikaty))
   
   wsk4 = wsk4 %>%
-    filter(.data$rok_abs %in% (rok))
+    filter(.data$rok_abs %in% (rok_abso))
   wsk3 = wsk3 %>%
-    filter(.data$rok_abs %in% (rok))
+    filter(.data$rok_abs %in% (rok_abso))
   
   if (duplikaty) {
     dups = NULL
@@ -185,19 +185,19 @@ agreguj_aneks_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok, duplikaty = TRUE) 
     l_kobiet = l_kobiet(.data))
   
   wskazniki_3 = agreguj_wskazniki(
-    wsk3, grupy, list("rok" = rok, "dups" = dups),
-    S3_12 = S3_mies(.data, min(rok), 12, max(rok), 12, dups),
-    tab_s3_zaw = zawody_S3(.data, min(rok), 12, max(rok), 12, dups),
-    E2_nauka_kontyn = E2_nauka_kontyn(.data, rok, 12),
-    Z4_ucz = Z4_ods_prac_mies(.data, min(rok), 9, max(rok), 12, TRUE),
-    Z4_nie_ucz = Z4_ods_prac_mies(.data, min(rok), 9, max(rok), 12, FALSE),
-    Z8_formy_ucz = Z8_formy_prac_mies(.data, rok, 12, TRUE),
-    Z8_formy_nie_ucz = Z8_formy_prac_mies(.data, rok, 12, FALSE),
-    Z9_mlod_wrz = Z9_kont_mlod(.data, rok, 9),
-    W3_ucz = W3_sr_doch_uop(.data, rok, 9, 12, TRUE),
-    W3_nie_ucz = W3_sr_doch_uop(.data, rok, 9, 12, FALSE),
-    B2_bezrob = B2_ods_bezrob(.data, rok, 9, 12),
-    N2_biernosc = N2_ods_biernosc(.data, rok, 9, 12)
+    wsk3, grupy, list("rok_abso" = rok_abso, "dups" = dups),
+    S3_12 = S3_mies(.data, min(rok_abso), 12, max(rok_abso), 12, dups),
+    tab_s3_zaw = zawody_S3(.data, min(rok_abso), 12, max(rok_abso), 12, dups),
+    E2_nauka_kontyn = E2_nauka_kontyn(.data, rok_abso, 12),
+    Z4_ucz = Z4_ods_prac_mies(.data, min(rok_abso), 9, max(rok_abso), 12, TRUE),
+    Z4_nie_ucz = Z4_ods_prac_mies(.data, min(rok_abso), 9, max(rok_abso), 12, FALSE),
+    Z8_formy_ucz = Z8_formy_prac_mies(.data, rok_abso, 12, TRUE),
+    Z8_formy_nie_ucz = Z8_formy_prac_mies(.data, rok_abso, 12, FALSE),
+    Z9_mlod_wrz = Z9_kont_mlod(.data, rok_abso, 9),
+    W3_ucz = W3_sr_doch_uop(.data, rok_abso, 9, 12, TRUE),
+    W3_nie_ucz = W3_sr_doch_uop(.data, rok_abso, 9, 12, FALSE),
+    B2_bezrob = B2_ods_bezrob(.data, rok_abso, 9, 12),
+    N2_biernosc = N2_ods_biernosc(.data, rok_abso, 9, 12)
   )
   
   wskazniki_4$grupy = wskazniki_4$grupy %>%
@@ -219,8 +219,8 @@ agreguj_aneks_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok, duplikaty = TRUE) 
 #' rundy monitoringu na danych administracyjnych
 #' @param grupy ramka danych zawierająca definicje podziałów na grupy -
 #' np. zwrócona przez funkcję \code{\link{utworz_grupowanie_ze_zmiennej}}
-#' @param rok rok, w którym grupa absolwentów uzyskała status absolwenta. Może
-#' to być więcej niż 1 wartość.
+#' @param rok_abso rok, w którym grupa absolwentów uzyskała status absolwenta.
+#' Może to być więcej niż 1 wartość.
 #' @param duplikaty wartość logiczna określająca czy należy odfiltrować ze
 #' zbioru duplikaty przy liczeniu niektórych wskaźników. Przyjmuje wartości:
 #' \itemize{
@@ -238,19 +238,19 @@ agreguj_aneks_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok, duplikaty = TRUE) 
 #' }
 #' @export
 #' @importFrom dplyr %>% filter .data left_join
-agreguj_szkozaw_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok, duplikaty = TRUE) {
+agreguj_szkozaw_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok_abso, duplikaty = TRUE) {
   stopifnot(is.data.frame(wsk3),
             is.data.frame(wsk4),
             is.data.frame(grupy),
-            rok %in% c(2020, 2021) & length(rok) %in% 1,
+            rok_abso %in% c(2020, 2021) & length(rok_abso) %in% 1,
             c("id_szk", "id_abs", "rok_abs", "typ_szk", "teryt_woj") %in% names(wsk3),
             c("id_szk", "id_abs", "rok_abs", "typ_szk", "teryt_woj") %in% names(wsk4),
             is.logical(duplikaty))
   
   wsk4 = wsk4 %>%
-    filter(.data$rok_abs %in% (rok))
+    filter(.data$rok_abs %in% (rok_abso))
   wsk3 = wsk3 %>%
-    filter(.data$rok_abs %in% (rok))
+    filter(.data$rok_abs %in% (rok_abso))
   
   if (duplikaty) {
     dups = NULL
@@ -267,9 +267,9 @@ agreguj_szkozaw_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok, duplikaty = TRUE
     licz_zawody = licz_zawody(.data))
   
   wskazniki_3 = agreguj_wskazniki(
-    wsk3, grupy, list("rok" = rok, "dups" = dups),
-    S3_12 = S3_mies(.data, min(rok), 12, max(rok), 12, dups),
-    tab_s3_zaw = zawody_S3(.data, min(rok), 12, max(rok), 12, dups)
+    wsk3, grupy, list("rok_abso" = rok_abso, "dups" = dups),
+    S3_12 = S3_mies(.data, min(rok_abso), 12, max(rok_abso), 12, dups),
+    tab_s3_zaw = zawody_S3(.data, min(rok_abso), 12, max(rok_abso), 12, dups)
   )
   
   wskazniki_4$grupy = wskazniki_4$grupy %>%
@@ -280,6 +280,3 @@ agreguj_szkozaw_1rokpo_adm_2 = function(wsk3, wsk4, grupy, rok, duplikaty = TRUE
   wskazniki = list(grupy = wskazniki_4$grupy, grupyOdniesienia = wskazniki_4$grupyOdniesienia)
   return(wskazniki)
 }
-
-
-
